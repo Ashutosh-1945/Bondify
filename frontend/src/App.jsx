5cd4f1c7a582429b66cc7120adc24e91a396c225
Ashutosh <ashutosh97771@gmail.com>
2025-03-05 23:08:38 +0530

    Fix line endings

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { Button, Container, Typography } from "@mui/material";
+
+function App() {
+  return (
+    <Container>
+      <Typography variant="h2" gutterBottom>
+        Welcome to My App
+      </Typography>
+      <Button variant="contained" color="primary">
+        Click Me
+      </Button>
+    </Container>
+  );
+}
+
+export default App;
